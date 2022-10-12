8af40cf924c18af49289701b19539b9a5ec9aac7
ld_xu <ld_xu@trip.com>
2022-10-12 10:58:10 +0800

    配合Helmet通过cdn方式加载

--- src/pages/other-babylon-doc/1_basic/index.js
+++ src/pages/other-babylon-doc/1_basic/index.js
@@ -1,39 +1,39 @@
 import { useEffect } from "react";
-import { Scene, Engine, FreeCamera, Vector3, HemisphericLight, MeshBuilder, StandardMaterial, Color3, Texture, SceneLoader } from "babylonjs";
+import { Helmet } from "react-helmet";
 import { Canvas } from "./index.style";
 import LandscapeImg from "@/imgs/public.jpeg";
 
 const Index = () => {
   const createScene = (canvas, engine) => {
-    const scene = new Scene(engine);
+    const scene = new BABYLON.Scene(engine);
 
-    const camera = new FreeCamera("camera", new Vector3(0, 5, -10), scene);
-    camera.setTarget(Vector3.Zero());
+    const camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
 
     camera.attachControl(canvas, true);
 
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
     // 创建小球
-    const sphere = new MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+    const sphere = new BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
     sphere.position.y = 1;
 
-    const ground = new MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    const ground = new BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 
-    const groundMaterial = new StandardMaterial("Ground Material", scene);
+    const groundMaterial = new BABYLON.StandardMaterial("Ground Material", scene);
     ground.material = groundMaterial;
-    // ground.material.diffuseColor = Color3.Red();
+    // ground.material.diffuseColor = BABYLON.Color3.Red();
 
     // 将图片填充到ground上
-    let groundTexture = new Texture(LandscapeImg, scene);
+    let groundTexture = new BABYLON.Texture(LandscapeImg, scene);
     ground.material.diffuseTexture = groundTexture;
 
-    // SceneLoader.ImportMesh('', "/", 'ss', scene, function(newMeshes) {
+    // BABYLON.SceneLoader.ImportMesh('', "/", 'ss', scene, function(newMeshes) {
     //   newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
     // })
 
-    // SceneLoader.ImportMesh("", Assets.meshes.Yeti.rootUrl, Assets.meshes.Yeti.filename, scene, function (newMeshes) {
+    // BABYLON.SceneLoader.ImportMesh("", Assets.meshes.Yeti.rootUrl, Assets.meshes.Yeti.filename, scene, function (newMeshes) {
     //   newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
     // });
 
@@ -41,16 +41,24 @@ const Index = () => {
   };
 
   useEffect(() => {
-    const canvas = document.querySelector("canvas");
-    const engine = new Engine(canvas);
-    const scene = createScene(canvas, engine);
-
-    engine.runRenderLoop(() => {
-      scene.render();
-    });
+    // !!! 这里配合Helmet通过cdn方式加载 需要setTimeout等待一会资源加载  有隐患
+    setTimeout(() => {
+      console.log(window, window.BABYLON);
+      const canvas = document.querySelector("canvas");
+      const engine = new BABYLON.Engine(canvas);
+      const scene = createScene(canvas, engine);
+  
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
+    }, 60);
   }, []);
+
   return (
     <div>
+      <Helmet>
+        <script src="https://cdn.babylonjs.com/babylon.js" type="text/javascript" />
+      </Helmet>
       <p>Babylon 基础 Doc</p>
 
       <Canvas></Canvas>
